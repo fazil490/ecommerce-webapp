9e81eee1fb804b951314b1f4027d4919d880e24a
Fazil <mdif4900@gmail.com>
2023-10-16 11:35:34 +0530

    pages

---
+++ src/pages/wishlist/Wishlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const Wishlist = () => {
+  return (
+    <div>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default Wishlist
